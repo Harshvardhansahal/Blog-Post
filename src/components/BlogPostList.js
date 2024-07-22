429fab1315bd9be6d833ec9b5305c691e4ee7616
harshvardhan sahal <harshvardhansahal@gmail.com>
2024-07-22 21:39:00 +0530

    Added pages size

--- src/components/BlogPostList.js
+++ src/components/BlogPostList.js
@@ -5,18 +5,22 @@ import BlogPostItem from './BlogPostItem';
 const BlogPostList = () => {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  const pageSize = 14;
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const result = await axios.get(`https://newsapi.org/v2/everything`, {
+        const result = await axios.get('https://newsapi.org/v2/everything', {
           params: {
             q: 'technology',
             apiKey: 'a089d8a237f641cf81e63e77e3097b3e',
-            page: page
+            page: page,
+            pageSize: pageSize
           }
         });
         setPosts(result.data.articles.filter(post => post.urlToImage));
+        setTotalResults(result.data.totalResults);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -38,16 +42,15 @@ const BlogPostList = () => {
         ))}
       </div>
       <div className="pagination d-flex justify-content-between mt-4">
-        <button onClick={() => setPage(page - 1)} disabled={page === 1} className="btn btn-secondary">
+        <button onClick={() => setPage(page - 1)} disabled={page === 1} className="btn btn-secondary mb-2">
           Previous
         </button>
-        <button onClick={() => setPage(page + 1)} className="btn btn-secondary">
+        <button onClick={() => setPage(page + 1)} disabled={page * pageSize >= totalResults} className="btn btn-secondary mb-2">
           Next
         </button>
       </div>
     </div>
   );
 };
-
 
 export default BlogPostList;
